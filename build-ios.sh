9db1acdebd6a6b754087753356c14dfec1d8b1d1
Nick Ochiel <nochiel@users.noreply.github.com>
2021-09-17 06:24:11 +0300

    fixup! Sync with breezmobile's workflows/main.yml.

--- build-ios.sh
+++ build-ios.sh
@@ -4,5 +4,7 @@
 #export GO111MODULE=off
 mkdir -p build/ios
 time {
-    PATH=$PATH:$GOPATH/bin gomobile bind -target=ios/arm64 -tags="ios experimental signrpc walletrpc chainrpc invoicesrpc routerrpc backuprpc peerrpc submarineswaprpc breezbackuprpc" -o build/ios/bindings.framework github.com/breez/breez/bindings
+    PATH=$PATH:$GOPATH/bin gomobile bind -target=ios -tags="ios experimental signrpc walletrpc chainrpc invoicesrpc routerrpc backuprpc peerrpc submarineswaprpc breezbackuprpc" -o build/ios/bindings.framework -ldflags="-s -w" github.com/breez/breez/bindings
+    
+    # PATH=$PATH:$GOPATH/bin gomobile bind -target=ios -tags="ios experimental signrpc walletrpc chainrpc invoicesrpc rout errpc backuprpc peerrpc submarineswaprpc breezbackuprpc" -o build/ios/bindings.xcframework -ldflags="-s -w" github.com/breez/breez/bindings
 }
